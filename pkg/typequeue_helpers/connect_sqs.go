be28be59cb234d207425771c03fd12293e416d80
Kenton Vizdos <kvizdos@protonmail.com>
2025-02-19 11:53:46 -0500

    Add Connect to SQS helper

---
+++ pkg/typequeue_helpers/connect_sqs.go
@@ -0,0 +1,20 @@
+package typequeue_helpers
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+)
+
+func ConnectToSQS(region string) (*sqs.SQS, error) {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(region),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	sqsClient := sqs.New(sess)
+
+	return sqsClient, nil
+}
